84de4132080fac5a13111e551fd6a5da2cc549e0
Edivan Silva <codedavinci.now@gmail.com>
2020-02-03 05:11:12 -0500

    feat(DegreeConversions): Add utilities to support temperature convesions (Kelvin, Farenheit, Celsius

---
+++ src/utils/degreeConversion.js
@@ -0,0 +1,5 @@
+
+export const kelvinToCelsius = (deg) => Math.round(deg - 273.15)
+export const kelvinToFarenheit = (deg) => (kelvinToCelsius(deg) * 9 / 5) + 32
+export const farenheitToCelsius = (deg) => Math.round(deg - 32) * 5 / 9
+export const celsiusToFarenheit = (deg) => Math.round(deg * 9 / 5) + 32
