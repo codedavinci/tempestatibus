a05bf6ca46e3934096a113a26c6da5fdf2942ea8
Edivan Silva <codedavinci.now@gmail.com>
2020-02-03 05:12:04 -0500

    refactor(weatherApi): Refactor weatherApi to become more functional for better testability

--- src/api/weatherApi.js
+++ src/api/weatherApi.js
@@ -1,58 +1,68 @@
 
 import format from 'date-fns/format'
+import axios from 'axios'
 
 import safeGet from '../utils/safeGet'
+import { kelvinToCelsius } from '../utils/degreeConversion'
 
-const GET_ENDPOINT = (place) => `https://api.openweathermap.org/data/2.5/forecast?q=${place},ca&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
-
-
-const normalizeForecasts = (forecasts) => {
-
-  const normalizedData = forecasts.list.map(value => {
-
-    let icon = safeGet(['weather', 0, 'icon'], value)
-    let date = safeGet(['dt_txt'], value).split(" ")[0]
-
-    let formatedDate = format(new Date(date), 'EEEE')
-
-    return {
-      id: safeGet(['dt'], value),
-      description: safeGet(['weather', 0, 'description'], value),
-      humidity: safeGet(['main', 'humidity'], value),
-      icon: `http://openweathermap.org/img/wn/${icon}@2x.png`,
-      temperature: Math.floor(safeGet(['main', 'temp'], value) - 273.15),
-      feelsLike: Math.floor(safeGet(['main', 'feels_like'], value) - 273.15),
-      wind: safeGet(['wind', 'speed'], value),
-      date: formatedDate
-    }
-  })
-    .reduce((acc, current) => {
-      const found = acc.find(item => item.date === current.date);
-
-      if (!found) {
-        return acc.concat([current]);
-      } else {
-        return acc;
-      }
-    }, [])
-    .slice(0, 5);
+export const GET_ENDPOINT = (place) => `https://api.openweathermap.org/data/2.5/forecast?q=${place},ca&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
+export const GET_ICONS_ENDPOIND = (icon) => `http://openweathermap.org/img/wn/${icon}@2x.png`
 
 
-  let city = safeGet(['city'], forecasts)
+export const normalizeForecasts = (forecast) => {
+
+  let icon = safeGet(['weather', 0, 'icon'], forecast)
+  let date = safeGet(['dt_txt'], forecast).split(" ")[0]
+
+  let formatedDate = format(new Date(date), 'EEEE')
 
   return {
-    city: `${city.name}, ${city.country}`,
-    days: [...normalizedData]
+    id: safeGet(['dt'], forecast),
+    description: safeGet(['weather', 0, 'description'], forecast),
+    humidity: safeGet(['main', 'humidity'], forecast),
+    icon: GET_ICONS_ENDPOIND(icon),
+    temperature: kelvinToCelsius(safeGet(['main', 'temp'], forecast)),
+    feelsLike: kelvinToCelsius(safeGet(['main', 'feels_like'], forecast)),
+    wind: safeGet(['wind', 'speed'], forecast),
+    date: formatedDate
   }
-
 }
 
+export const removeDuplicatesByDate = (normalizedForecasts) => {
+  return normalizedForecasts.reduce((acc, current) => {
+    const found = acc.find(item => item.date === current.date);
+
+    if (!found) {
+      return acc.concat([current]);
+    } else {
+      return acc;
+    }
+  }, [])
+}
+
+export const getOnlyNextFiveDays = (forecasts) => forecasts.slice(0, 5)
+
+
+export const getForecasts = async (place) => {
+
+  const { data } = await axios.get(GET_ENDPOINT(place))
+
+  return data
+}
 
 
 export const fetchForecasts = async (place) => {
 
-  const res = await fetch(GET_ENDPOINT(place))
-  const data = await res.json()
+  const rawForecasts = await getForecasts(place)
 
-  return normalizeForecasts(data)
+  const city = safeGet(['city'], rawForecasts)
+  const normalized = rawForecasts.map(normalizeForecasts)
+  const dedupedForecasts = removeDuplicatesByDate(normalized)
+  const nextFiveDaysFromToday = getOnlyNextFiveDays(dedupedForecasts)
+
+  return {
+    city: `${city.name}, ${city.country}`,
+    days: [...nextFiveDaysFromToday]
+  }
+
 }
